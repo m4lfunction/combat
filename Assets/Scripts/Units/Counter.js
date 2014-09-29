b9c6592fd99c33bb319e9009d87da5e7f891ffc6
Daniel Pleske <daniel.pleske@gmail.com>
2014-09-30 01:59:58 +0200

    added squad based combat

--- Assets/Scripts/Units/Counter.js
+++ Assets/Scripts/Units/Counter.js
@@ -3,6 +3,7 @@
 var hp : int = 10;
 var attackSpeed : float = 2;
 var attackDistance : float = 2;
+var aggroRange : float = 10;
 
 function Start () {
 
--- Assets/Scripts/Units/Goblin/GoblinAI.js
+++ Assets/Scripts/Units/Goblin/GoblinAI.js
@@ -3,9 +3,12 @@
 var inCombat : boolean = false;
 var fighting : boolean = false;
 
+var flag : GameObject;
+
 private var attackSpeed : float;
 private var nextAttackIn : float;
 private var attackDistance : float;
+private var aggroRange : float;
 
 private var agent : NavMeshAgent;
 private var lastFramePosition: Vector3;
@@ -13,10 +16,13 @@ private var target : GameObject;
 
 function Start () {
 
+	flag = GameObject.Find("Flag");
+
 	nextAttackIn = Time.time;
 
 	attackSpeed = gameObject.GetComponent(Counter).attackSpeed;
 	attackDistance = gameObject.GetComponent(Counter).attackDistance;
+	aggroRange = gameObject.GetComponent(Counter).aggroRange;
 
 	agent = GetComponent.<NavMeshAgent>();
 	
@@ -48,25 +54,37 @@ function Update () {
 	}
 
 	// Move character
-	var hit: RaycastHit;
+	//var hit: RaycastHit;
 	// When the mouse is clicked...	
-	if (Input.GetMouseButtonDown(1)) {
+	//if (Input.GetMouseButtonDown(1)) {
 		// If the click was on an object then set the agent's
 		// destination to the point where the click occurred.
-		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		
-		if (Physics.Raycast(ray, hit)) {
-			if(hit.collider.tag == "Enemy"){
-				agent.SetDestination(hit.transform.position);
-				target = hit.transform.gameObject;
-				inCombat = true;
-			}else{
-				agent.SetDestination(hit.point);
-				inCombat = false;
-			}
-		}
+		//if (Physics.Raycast(ray, hit)) {
+			//if(hit.collider.tag == "Enemy"){
+				//agent.SetDestination(hit.transform.position);
+				//target = hit.transform.gameObject;
+				//inCombat = true;
+			//}else{
+				//agent.SetDestination(hit.point);
+				//inCombat = false;
+			//}
+		//}
+	//}
+	if(inCombat == false){
+		agent.SetDestination(flag.transform.position);
+	}else{
+		agent.SetDestination(target.transform.position);
 	}
-
+	
+	var dist = Vector3.Distance(FindClosestEnemy().transform.position, transform.position);
+	if (dist <= aggroRange){
+		inCombat = true;
+		target = FindClosestEnemy();
+	}
+	
+	
 	// Combat
 	if(inCombat == true){
 		if(fighting == false){
@@ -89,4 +107,23 @@ function Update () {
 			}
 		}
 	}
+}
+
+function FindClosestEnemy () : GameObject {
+	// Find all game objects with tag Enemy
+	var gos : GameObject[];
+	gos = GameObject.FindGameObjectsWithTag("Enemy"); 
+	var closestEnemy : GameObject; 
+	var distance = Mathf.Infinity; 
+	var position = transform.position; 
+	// Iterate through them and find the closest one
+	for (var go : GameObject in gos)  { 
+		var diff = (go.transform.position - position);
+		var curDistance = diff.sqrMagnitude; 
+		if (curDistance < distance) { 
+			closestEnemy = go; 
+			distance = curDistance; 
+		} 
+	} 
+	return closestEnemy;
 }
