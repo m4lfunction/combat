34136abaedc180a6b881e1fb5bed721a776ba983
Daniel Pleske <daniel.pleske@gmail.com>
2014-10-02 23:18:10 +0200

    added level 1

--- Assets/Scripts/Menus/Chooselevel.js
+++ Assets/Scripts/Menus/Chooselevel.js
@@ -5,6 +5,6 @@ function OnGUI() {
 		Application.LoadLevel("Test");
 	}
 	if (GUI.Button(Rect(10,110,200,30),"Level 1")){
-		//Application.LoadLevel("Level1");
+		Application.LoadLevel("Level1");
 	}
 }
